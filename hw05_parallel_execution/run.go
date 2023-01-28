861a1c06f613f0214a0059bcee4b2c97206cce04
Aleksandr.Yarovov <aleksandr.yarovov@pochtavip.com>
2023-01-28 21:12:24 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,86 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var (
+	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+	ErrInvalidJobs         = errors.New("invalid count jobs")
+)
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+type ErrCount struct {
+	mu    sync.RWMutex
+	count int
+}
+
+func (err *ErrCount) Count() int {
+	err.mu.RLock()
+	defer err.mu.RUnlock()
+
+	return err.count
+}
+
+func (err *ErrCount) Decrement() {
+	err.mu.Lock()
+	defer err.mu.Unlock()
+
+	err.count--
+}
+
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if n < 1 {
+		return ErrInvalidJobs
+	}
+
+	if m < 1 {
+		return ErrErrorsLimitExceeded
+	}
+
+	if len(tasks) < 1 {
+		return nil
+	}
+
+	wg := sync.WaitGroup{}
+	errCount := ErrCount{
+		count: m,
+	}
+
+	ch := make(chan Task, len(tasks))
+
+	for _, task := range tasks {
+		ch <- task
+	}
+
+	close(ch)
+
+	job := func(ch <-chan Task) {
+		defer wg.Done()
+
+		for task := range ch {
+			if errCount.Count() < 1 {
+				return
+			}
+
+			if err := task(); err != nil {
+				errCount.Decrement()
+			}
+		}
+	}
+
+	wg.Add(n)
+
+	for i := 0; i < n; i++ {
+		go job(ch)
+	}
+
+	wg.Wait()
+
+	if errCount.Count() < 1 {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
