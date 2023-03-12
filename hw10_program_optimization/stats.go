96e8e50d8a8a2d0bde275a519ae7416c2ab8d5fc
Aleksandr.Yarovov <aleksandr.yarovov@pochtavip.com>
2023-03-12 19:45:26 +0300

    HW10 revert easy json

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -7,7 +7,7 @@ import (
 	"regexp"
 	"strings"
 
-	jsoniter "github.com/json-iterator/go"
+	"github.com/mailru/easyjson"
 )
 
 type User struct {
@@ -23,7 +23,6 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	json := jsoniter.ConfigCompatibleWithStandardLibrary
 	s := bufio.NewScanner(r)
 	user := &User{}
 	result := make(DomainStat)
@@ -31,7 +30,7 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	reg, _ := regexp.Compile("(.*)@[^.]+\\." + domain)
 
 	for s.Scan() {
-		if err := json.Unmarshal(s.Bytes(), user); err != nil {
+		if err := easyjson.Unmarshal(s.Bytes(), user); err != nil {
 			return nil, fmt.Errorf("error unmarshal user: %w", err)
 		}
 
