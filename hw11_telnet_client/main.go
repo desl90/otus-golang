b7d0c42b7ef90d47dfdc9abd0ec0b84719111415
Aleksandr.Yarovov <aleksandr.yarovov@pochtavip.com>
2023-04-02 14:24:49 +0300

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,142 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"regexp"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/spf13/pflag"
+)
+
+var (
+	ErrWrongHost = errors.New("wrong host")
+	ErrWrongPort = errors.New("wrong port")
+)
+
+type config struct {
+	timeout time.Duration
+	host    string
+	port    string
+	socket  string
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	config, err := initConfig(getHostArg(), getPortArg(), getTimeoutArg())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := NewTelnetClient(config.socket, config.timeout, os.Stdin, os.Stdout)
+	if err := client.Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(client TelnetClient) {
+		err := client.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(client)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGQUIT, syscall.SIGINT)
+
+	go func() {
+		defer cancel()
+		if err := client.Send(); err != nil {
+			_, err := fmt.Fprint(os.Stderr, err)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	go func() {
+		defer cancel()
+		if err := client.Receive(); err != nil {
+			_, err := fmt.Fprint(os.Stderr, err)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	<-ctx.Done()
+}
+
+func initConfig(host, port string, timeout time.Duration) (*config, error) {
+	c := config{}
+
+	host, err := validateHost(host)
+	if err != nil {
+		return &c, err
+	}
+
+	port, err = validatePort(port)
+	if err != nil {
+		return &c, err
+	}
+
+	c.timeout = timeout
+	c.host = host
+	c.port = port
+	c.socket = net.JoinHostPort(c.host, c.port)
+
+	return &c, nil
+}
+
+func validateHost(host string) (string, error) {
+	if host == "" {
+		return "", ErrWrongHost
+	}
+
+	pattern := `^[a-zA-Z\d]{1}[a-zA-Z\d\.\-]+$`
+
+	matched, _ := regexp.MatchString(pattern, host)
+	if !matched {
+		return "", ErrWrongHost
+	}
+
+	return host, nil
+}
+
+func validatePort(port string) (string, error) {
+	if port == "" {
+		return "", ErrWrongPort
+	}
+
+	tcpPort, err := strconv.Atoi(port)
+	if err != nil {
+		return "", err
+	}
+
+	if tcpPort < 1 || tcpPort > 65535 {
+		return "", ErrWrongPort
+	}
+
+	return port, nil
+}
+
+func getTimeoutArg() time.Duration {
+	var timeout time.Duration
+
+	pflag.DurationVarP(&timeout, "timeout", "t", 10*time.Second, "")
+	pflag.Parse()
+
+	return timeout
+}
+
+func getPortArg() string {
+	return pflag.Arg(1)
+}
+
+func getHostArg() string {
+	return pflag.Arg(0)
 }
